6a45ee638d46e28fb129418153fbd68d1216420a
Justin Grayman <justin.grayman@gmail.com>
2021-07-11 00:37:26 -0500

    Adopted load balancer and ASG for simple web

--- vpc_prod_us_east_2_project_onica.tf
+++ vpc_prod_us_east_2_project_onica.tf
@@ -9,31 +9,66 @@ provider "aws" {
   region                        = "us-east-2"
 }
 
-
 resource "aws_vpc" "simple_web" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
-    Name = "simple_web"
+    Name       = "simple_web"
     networking = "terraform"
   }
 }
 
+resource "aws_internet_gateway" "sw_igw" { 
+    vpc_id =  aws_vpc.simple_web.id
+}
+
+resource "aws_subnet" "sw_az1" {
+  vpc_id            = aws_vpc.simple_web.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "us-east-2a"
+  
+  tags = {
+    networking = "terraform"
+  }
+}
+
+resource "aws_subnet" "sw_az2" {
+  vpc_id            = aws_vpc.simple_web.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-2b"
+  tags = {
+    networking = "terraform"
+  }
+}
+
+resource "aws_subnet" "sw_az3" {
+  vpc_id            = aws_vpc.simple_web.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-2c"
+  
+  tags = {
+    networking = "terraform"
+  }
+}
+
+resource "aws_route_table" "sw_routes" {
+  vpc_id = aws_vpc.simple_web.id
+  
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.sw_igw.id
+     }
+ }
+
 resource "aws_security_group" "simple_web" {
   name 		= "simple_web"
+  vpc_id        = aws_vpc.simple_web.id
   description   = "Sets rules to allow HTTP and HTTPS inbound traffic"
 
   ingress {
     from_port 	= 80
     to_port   	= 80
-    protocol  	= "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port 	= 443
-    to_port   	= 443
     protocol  	= "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -50,3 +85,64 @@ resource "aws_security_group" "simple_web" {
   }
 
 }
+
+resource "aws_elb" "simple_web" {
+  name            = "simple-web"
+  subnets         = [aws_subnet.sw_az1.id, aws_subnet.sw_az2.id, aws_subnet.sw_az3.id]
+  security_groups = [aws_security_group.simple_web.id]
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+    target              = "TCP:80"
+    interval            = 30
+  }
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  tags = {
+    load_balancing = "terraform"
+  }
+
+}
+
+resource "aws_launch_template" "simple_web" {
+  name_prefix   = "simple-web"
+  image_id      = "ami-0fd02a3bc174c9398"
+  instance_type = "t2.micro"
+
+  tags = {
+    auto_scaling = "terraform"
+  }
+}
+
+resource "aws_autoscaling_group" "simple_web" {
+  vpc_zone_identifier = [aws_subnet.sw_az1.id, aws_subnet.sw_az2.id, aws_subnet.sw_az3.id]
+  desired_capacity    = 2
+  max_size            = 3
+  min_size            = 1
+
+  launch_template {
+    id      = aws_launch_template.simple_web.id
+    version = "$Latest"
+  }
+
+  tag {
+    key 		= "auto_scaling"
+    value 		= "terraform"
+    propagate_at_launch = true
+  }
+
+}
+
+resource "aws_autoscaling_attachment" "simple_web" {
+  autoscaling_group_name = aws_autoscaling_group.simple_web.id
+  elb                    = aws_elb.simple_web.id
+}
+
